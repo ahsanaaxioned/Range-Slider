25dd2d3971164f22ec51f24c3450c57396ada8f9
ahsanaaxioned <ahsana@axioned.com>
2022-12-27 21:01:19 +0530

    js file added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,24 @@
-/* Author: 
+// global variable start here
 
-*/
+const inputOne = document.querySelector(".input"),
+  rangeval = document.querySelector(".input-value"),
+  inputTwo = document.querySelector(".input2"),
+  rangeval2 = document.querySelector(".input-value2");
 
+// global variable end here
 
+// function and event for both input range start here
 
+inputOne.value = 0;
+inputTwo.value = 0;
 
+setInterval(() => {
+  inputOne.value++;
+  rangeval.innerText = inputOne.value;
+}, 100);
 
+inputTwo.addEventListener("input", () => {
+  rangeval2.innerText = inputTwo.value;
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// function and event for both input range end here
